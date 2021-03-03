fd2f03826b6d76f3375d5c7f6cc15a76db9b1dc7
Natalia.Kukushkina <anpu01@yandex.ru>
2021-03-03 10:12:40 +0500

    Лабиринт

---
+++ proj-l/Window.h
@@ -0,0 +1,44 @@
+/*
+ * Window.h
+ *
+ *  Created on: 22 февр. 2021 г.
+ *      Author: AnPu0
+ */
+
+#ifndef WINDOW_H_
+#define WINDOW_H_
+
+#include <memory>
+#include <iostream>
+#include <stdexcept>
+#include <SDL2/SDL.h>
+
+class Window
+{
+public:
+	static constexpr int DEFAULT_WIDTH = 1366;
+	static constexpr int DEFAULT_HEIGHT = 768;
+
+protected:
+	std::shared_ptr<SDL_Window> _window;
+	std::shared_ptr<SDL_Renderer> _renderer;
+
+	int _width, _height;
+
+public:
+	Window(int width = DEFAULT_WIDTH, int height = DEFAULT_HEIGHT);
+	virtual ~Window() = default;
+
+	int width() const { return _width; }
+	int height() const { return _height; }
+
+	void main_loop();
+
+	virtual void setup() { }
+	virtual void handle_event(const SDL_Event & event) {}
+	virtual void handle_keys(const Uint8 * keys) {}
+	virtual void do_logic() { }
+	virtual void render() { }
+};
+
+#endif /* WINDOW_H_ */
